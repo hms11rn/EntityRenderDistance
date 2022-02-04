55e950e387a963cd161bb8cb623bf1614f3c2a2e
hms11rn <hmselik@gmail.com>
2022-02-04 13:59:14 +0200

    Changed version

--- src/main/java/com/github/hms11rn/EntityRenderDistance.java
+++ src/main/java/com/github/hms11rn/EntityRenderDistance.java
@@ -17,7 +17,7 @@ import net.minecraftforge.fml.common.registry.EntityRegistry;
 @Mod(modid = EntityRenderDistance.MODID, version = EntityRenderDistance.VERSION)
 public class EntityRenderDistance {
 	public static final String MODID = "erd";
-	public static final String VERSION = "2.0";
+	public static final String VERSION = "0.9";
 	public static EntityRenderDistance erd;
 	public static Config config;
 
